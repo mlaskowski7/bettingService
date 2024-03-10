cbbdedf509b7a478fba6bbfe73caba7f01e7d317
Mateusz Laskowski <mlaskowski7>
2024-03-10 13:35:27 +0100

    initializing spring boot app

---
+++ bettingServiceBackend/src/main/java/com/example/bettingServiceBackend/BettingServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bettingServiceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingServiceBackendApplication.class, args);
+	}
+
+}
