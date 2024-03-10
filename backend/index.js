003eeb93686deffd5f11601e4760f308634005f0
Mateusz Laskowski <mlaskowski7>
2024-03-10 16:26:16 +0100

    created backend user auth with postgres db and node js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,83 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
+const { Pool } = require("pg");
+
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+(async () => {
+  const client = await pool.connect();
+
+  try {
+    const { rows } = await client.query("SELECT current_user");
+    const currentUser = rows[0]["current_user"];
+    console.log(currentUser);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.release();
+  }
+})();
 
 app.use(express.json());
 
-const users = [];
+let users = [];
 
-app.get("/users", (request, response) => {
-  response.json(users);
+app.get("/users", async (request, response) => {
+  try {
+    const data = await pool.query('SELECT * FROM "user"');
+    users = data.rows;
+    response.status(200).send(data.rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send();
+  }
 });
 
-app.post("/users", (request, response) => {
-  const user = {
-    username: request.body.username,
-    password: request.body.password,
-  };
+app.post("/users", async (request, response) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(request.body.password, salt);
+    const user = {
+      username: request.body.username,
+      password: hashedPassword,
+    };
+    await pool.query(
+      'INSERT INTO "user" (username, password) VALUES ($1, $2)',
+      [user.username, user.password]
+    );
+    response.status(201).send(`Successfully added new user - ${user.username}`);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send();
+  }
+});
 
-  users.push(user);
-  response.json(user);
-  response.status(201);
+app.post("/users/login", async (request, response) => {
+  const user = users.find((user) => user.username == request.body.username);
+
+  if (user == null)
+    return response
+      .status(400)
+      .send("There is no user with provided username in the database");
+
+  try {
+    if (await bcrypt.compare(request.body.password, user.password)) {
+      response.send("Successfully Logged In");
+    } else {
+      response.send("Wrong Password");
+    }
+  } catch {
+    response.status(500).send();
+  }
 });
 
 app.listen(3000);
