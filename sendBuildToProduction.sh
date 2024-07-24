0dd4ef9d4516a9f6ff74f5bf7f310ecfe3e58648
Mateusz Laskowski <144243838+mlaskowski7@users.noreply.github.com>
2024-07-24 14:31:17 +0200

    Update sendBuildToProduction.sh

--- sendBuildToProduction.sh
+++ sendBuildToProduction.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-scp frontend/dist/*.png root@181.215.68.250:/root/frontendBuild
-scp frontend/dist/*.html root@181.215.68.250:/root/frontendBuild
-scp frontend/dist/assets/* root@181.215.68.250:/root/frontendBuild/assets
 
